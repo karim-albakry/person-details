6cff6dfd8a8de867a9470200c2c1d8c6b0348ce3
karim.albakry <karim.albakry@hotmail.com>
2025-01-31 17:42:39 +0200

    test: add unit tests for csv files

--- person-details-be/__tests__/CsvPersonRepository.test.ts
+++ person-details-be/__tests__/CsvPersonRepository.test.ts
@@ -16,6 +16,10 @@ describe("CsvPersonRepository (Test CSV File)", () => {
     expect(persons.length).toBeGreaterThan(0);
     expect(persons[0]).toHaveProperty("first name");
     expect(persons[0]).toHaveProperty("last name");
+    expect(persons[0]).toHaveProperty("telephone code");
+    expect(persons[0]).toHaveProperty("telephone number");
+    expect(persons[0]).toHaveProperty("address");
+    expect(persons[0]).toHaveProperty("country");
   });
 
   it("should filter persons by name", async () => {
@@ -33,5 +37,31 @@ describe("CsvPersonRepository (Test CSV File)", () => {
   it("should return empty if no match is found", async () => {
     const persons = await csvRepo.getPersons("Unknown Name");
     expect(persons.length).toBe(0);
+  });
+
+  it("should return empty array when CSV file is empty", async () => {
+    const originalFilePath = process.env.CSV_FILE_PATH;
+    process.env.CSV_FILE_PATH = "./__tests__/data/empty.csv";
+
+    const persons = await csvRepo.getPersons();
+    expect(persons).toEqual([]);
+
+    process.env.CSV_FILE_PATH = originalFilePath
+  });
+
+  it("should handle missing fields in CSV", async () => {
+    const originalFilePath = process.env.CSV_FILE_PATH;
+    process.env.CSV_FILE_PATH = "./__tests__/data/missing_columns.csv";
+
+    const persons = await csvRepo.getPersons();
+    expect(persons.length).toBeGreaterThan(0);
+    expect(persons[0]["first name"]).toBeDefined();
+    expect(persons[0]["last name"]).toEqual("Amr");
+    expect(persons[0]["telephone number"]).toBeDefined();
+    expect(persons[0]["telephone number"]).toEqual("");
+    expect(persons[2]["first name"]).toEqual("");
+    expect(persons[4]["address"]).toEqual("");
+
+    process.env.CSV_FILE_PATH = originalFilePath
   });
 });
